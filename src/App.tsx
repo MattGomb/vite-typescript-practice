efef8664d792e1d77b74a38d66dcbec4392b856d
MattGomb <gombosonline@gmail.com>
2023-05-19 17:31:04 -0500

    Clean app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
-import Heading from "./components/Heading"
-import { Section } from "./components/Section"
-import Counter from "./components/Counter"
-import { useState } from "react";
-import List from "./components/List";
 
 function App() {
-  const [count, setCount] = useState<number>(0)
 
-  return (
-    <>
-      <Heading title="Hello, world!" />
-      <Section title="Chordle my balls">This is my Section.</Section>
-      <Counter setCount={setCount}>Count is {count}</Counter>
-      <List items={["alcohol", "bitches", "coding"]} render={(item: string) => <span className="bold gold">{item}</span>} />
-    </>
-  )
+  return 
+  
 }
 
 export default App
