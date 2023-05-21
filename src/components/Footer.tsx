10d5fbe0c8c93a860100df55a37d704f219052ed
MattGomb <gombosonline@gmail.com>
2023-05-21 14:05:12 -0500

    Create Header and Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,18 @@
-const Footer = () => {
+import useCart from "../hooks/useCart"
+
+type PropsType = {
+  viewCart: boolean,
+}
+
+const Footer = ({viewCart}: PropsType) => {
+  const {totalItems, totalPrice} = useCart()
+
+  const year: number = new Date().getFullYear()
+
+  const pageContent = viewCart ? <p>Shopping Cart &copy; {year}</p> : (<><p>Total items: {totalItems}</p><p>Total price: {totalPrice}</p><p>Shopping Cart &copy; {year}</p></>)
+  
   return (
-    <div>Footer</div>
+    <footer className="footer">{pageContent}</footer>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,27 @@
-const Header = () => {
-  return (
-    <div>Header</div>
+import Nav from "./Nav"
+import useCart from "../hooks/useCart"
+
+type PropsType = {
+  viewCart: boolean,
+  setViewCart: React.Dispatch<React.SetStateAction<boolean>>,
+}
+
+const Header = ({ viewCart, setViewCart }: PropsType) => {
+  const { totalItems, totalPrice } = useCart()
+
+  const Content  = (
+    <header className="header">
+      <div className="header__title-bar">
+        <h1>Acme co</h1>
+        <div className="header__price-box">
+          <p>Total items: {totalItems}</p>
+          <p>Total price: {totalPrice}</p>
+        </div>
+      </div>
+      <Nav viewCart={viewCart} setViewCart={setViewCart} />
+    </header>
   )
+  return Content
 }
 
 export default Header
